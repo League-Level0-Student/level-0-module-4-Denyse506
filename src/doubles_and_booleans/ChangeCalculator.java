0e777903fc7a7f551eaedecc92b58b52857cea8c
38558 <38558@0338S-S-R0B6ZHN.sdcs.local>
2018-10-23 14:24:30 -0700

    finshed

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -16,8 +16,16 @@ public class ChangeCalculator {
 
 		// Ask the user how many nickels they have
  String amt=JOptionPane.showInputDialog("how many nickels do you have");
-		int
- 
+		int am= Integer.parseInt(amt);
+ String dime =JOptionPane.showInputDialog("how many dimes do you have");
+       int ime=Integer.parseInt(dime);
+ String quarters=JOptionPane.showInputDialog("how many quarter do you have");
+ int arters=Integer.parseInt(quarters);
+ double money =0.0;
+ money+=0.05*am;
+ money+=0.10*ime;
+ money+=0.25*arters;
+System.out.println(money);
  // Convert their answer to an int using Integer.parseInt()
 
 		// Ask the user how many dimes they have, and convert their answer
