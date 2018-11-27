e670baa308fe877648a6e0293c1432222c76c0c0
38558 <38558@0338S-S-R0B6ZHP.sdcs.local>
2018-11-27 14:23:39 -0800

    working on them

--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -8,7 +8,7 @@ public class HappyPet {
 static	int happines=0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-String Type=JOptionPane.showInputDialog("what type of of pets will you have");
+String Type =JOptionPane.showInputDialog("what type of of pets will you have");
 	
 // 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
@@ -16,29 +16,47 @@ String Type=JOptionPane.showInputDialog("what type of of pets will you have");
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 
 			//    Make sure to customize the title and question too.
-			int ant = JOptionPane.showOptionDialog(null, "What will you doto show happpiness to your pet", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
+for (int i = 0; i < 10; i++) {
+	
+
+int ant = JOptionPane.showOptionDialog(null, "What will you do to show happpiness to your pet", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
 					new String[] { "cuddle", "feed", "groom" }, null);
-	}
-			static void feed() {
-				(
-			}
-			
-			static void cuddle() {
-				
-			}
-			static void groom() {
-				
-			}
-			
+	
+		
 			
 			// 5. Use user input to call the appropriate method created in step 4.
-
+		
+			if(ant==0) {
+        	 feed();
+         }else if(ant==1) {
+        	 cuddle();
+         } else if(ant==2) {
+        	 groom();
+         }
+}
+            
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+               for (int i = 0; i < args.length; i++) {
+				
+			}
 	
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	}
+static void feed() {
+	JOptionPane.showMessageDialog(null, "wag tail");
+	happines++;
+}
+
+static void cuddle() {
+	JOptionPane.showMessageDialog(null, "happy");
+	happines++;
+}
+static void groom() {
+	JOptionPane.showMessageDialog(null,"nice");
+	
+}
 }
